63b9350eafddb6e78aba7e4c306669ea25d080d9
Loulen <louis.lenoir@insa-rouen.fr>
2019-03-17 19:36:23 +0100

    ajout de la conversion MatriceCrypto en DataFrame

--- Matrice_de_donnes.py
+++ Matrice_de_donnes.py
@@ -1,10 +1,6 @@
 from getohlc import get_ohlc_data_date, get_data
 from datetime import datetime
-
-
-class MatriceCrypto:
-    """ Objet aggrégeant dans l'ordre chronologique des
-    """
+from pandas import DataFrame
 
 class Indicateurs:
     """Objet contenant les différents indicateurs :
@@ -13,8 +9,8 @@ class Indicateurs:
     - SMA
     ...
     """
-    def __init__(self, date, cours, rsi, sma):
-        self._dict={"date":date,"cours":cours,"rsi":rsi,"sma":sma}
+    def __init__(self, date, cours, rsi, sma, ema):
+        self._dict={"date":date,"cours":cours,"rsi":rsi,"sma":sma,"ema":ema}
 
     # ---------------getters / setters ----------
 
@@ -39,8 +35,30 @@ class MatriceCrypto:
         if (len(indicateurs)>1):
             for indicateur in indicateurs:
                 self.ajouter(indicateur)
+        elif (isinstance(indicateurs[0], list)):
+            for indicateur in indicateurs[0]:
+                self.ajouter(indicateur)
         else:
             self.ajouter(indicateurs[0])
 
+    # ---------------getters / setters ----------
+
+    def __getattr__(self, attr):
+        return self._donnees[attr]
+
+    # -------------------------------------------
+
     def ajouter(self, indicateur):
-        self._donnees.update({indicateur.date : indicateur})
+        self._donnees.update({indicateur.date : list(indicateur)[1:]})
+
+    @property
+    def matrice(self):
+        """renvoie une matrice d*s avec :
+        d : nombre de descripteurs
+        s : nombre de séances """
+        return DataFrame.from_dict(m._donnees, orient='index',columns=["cours", "rsi", "sma", "ema"])
+
+indic=[]
+for i in range(20):
+    indic.append(Indicateurs(datetime(2019, 3, 6+i, 18, 00, 34), 500+i, 20+i, 30+i, i))
+m=MatriceCrypto(indic)
