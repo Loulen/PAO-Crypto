4e64a57ec073fc81524219291ba17ef6a2d8aedf
Abadou Sofiane <sofiane.abadou@insa-rouen.fr>
2019-06-03 12:46:48 +0200

    Préparations des données pour le machineLearning

---
+++ Apprentissage.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+def Apprentissage():
+    for i in len(os.listdir('./2018')) :
+        reshape la matrice en un vecteur
+        chopper l outcome correspondant au dernier pourcent de vecteur
+        stocker le vecteur dans une matrice X, l outcome dans Y
+        BALANCE LE ML MON POTE
--- data_tocsv.py
+++ data_tocsv.py
@@ -5,10 +5,21 @@ import pandas as pd
 from indicateurs import ema, rsi
 import os
 import shutil
+from datetime import datetime
 
 
 def data_frame_annee(annee=2018, monnaie='BTC', ema_period=10, rsi_period=14, hours_per_file=168):
     data=get_price_hourly(monnaie, since='1/1/'+str(annee), to='1/1/'+str(annee+1))
+    month = []
+    day = []
+    weekday = []
+    hours = []
+    for hour in data.index :
+        date = datetime.fromtimestamp(hour)
+        month.append(date.month)
+        day.append(date.day)
+        hours.append(date.hour)
+        weekday.append(date.weekday())
     data=data[::-1]
     ema_data=[]
     rsi_data=[]
@@ -20,6 +31,12 @@ def data_frame_annee(annee=2018, monnaie='BTC', ema_period=10, rsi_period=14, ho
     rsi_data.extend(rsi(list(data['Prix']),period=rsi_period))
     data['ema'] = pd.Series(ema_data, index=data.index)
     data['rsi'] = pd.Series(rsi_data, index=data.index)
+    data['month'] = pd.Series(month, index=data.index)
+    data['weekday'] = pd.Series(weekday, index = data.index)
+    data['day'] = pd.Series(day, index=data.index)
+    data['hour'] = pd.Series(hours, index=data.index)
+
+
     # on retire les 250 premières valeurs pour tenir compte de l'actualisation du rsi
     data=data[rsi_period+250:]
     hours_left=len(data)
@@ -33,8 +50,7 @@ def data_frame_annee(annee=2018, monnaie='BTC', ema_period=10, rsi_period=14, ho
     while hours_left>hours_per_file:
         data[len(data)-hours_left:len(data)-hours_left+hours_per_file].to_csv('./'+str(annee)+'/'+str(file_number))
         file_number=file_number+1
-        hours_left=hours_left-hours_per_file
+        hours_left=hours_left-1
 
     return data
-
 
