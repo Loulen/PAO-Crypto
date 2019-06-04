ed911509dfd735bc80f0e12da01cac826800c625
Loulen <lnrlouis@gmail.com>
2019-06-04 13:02:32 +0200

    Co-authored-by: INSA-llenoir <louis.lenoir@insa-rouen.fr>

--- Apprentissage.py
+++ Apprentissage.py
@@ -1,9 +1,24 @@
 import pandas as pd
 
 
-def Apprentissage():
-    for i in len(os.listdir('./2018')) :
-        reshape la matrice en un vecteur
-        chopper l outcome correspondant au dernier pourcent de vecteur
-        stocker le vecteur dans une matrice X, l outcome dans Y
-        BALANCE LE ML MON POTE
+def apprentissage():
+        for i in len(os.listdir('./2018')) :
+                semaine = pd.read_csv('./2018/'+str(i))
+                (semaine,y)=get_outcome(semaine)
+                
+
+        
+        # récuperer 1% des données d outcome de la dataframe
+        # reshape la matrice en un vecteur
+        # chopper l outcome correspondant au dernier pourcent de vecteur
+        # stocker le vecteur dans une matrice X, l outcome dans Y
+        # BALANCE LE ML MON POTE
+
+
+def get_outcome(df):
+        outcome=[]
+        nb_valeurs =int((len(df.index)//100)+1)
+        cours = df['Prix']
+        df = df.iloc[:len(df.index)-nb_valeurs]
+        outcome = cours[-nb_valeurs:]
+        return (df,outcome) 
